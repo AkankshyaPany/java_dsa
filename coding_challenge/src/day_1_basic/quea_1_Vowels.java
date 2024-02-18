42dce6c18e9d9a546ae6dc6994b96313384ebe44
Akankshya Pany <panysayeni@gmail.com>
2024-02-18 14:44:29 +0530

    second commit

--- coding_challenge/src/day_1_basic/quea_1_Vowels.java
+++ coding_challenge/src/day_1_basic/quea_1_Vowels.java
@@ -1,10 +1,18 @@
 package day_1_basic;
-
+import java.util.*;
 public class quea_1_Vowels {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the character");
+		char ch = sc.next().charAt(0);
+		if(ch=='a'||ch=='e'||ch=='o'||ch=='i'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+		{
+			System.out.println("yes..vowel");
+		}else {
+			System.out.println("no .. nt a vowel");
+		}
 	}
 
 }
--- coding_challenge/src/day_1_numbers/ques_6_primenumber.java
+++ coding_challenge/src/day_1_numbers/ques_6_primenumber.java
@@ -8,7 +8,16 @@ public class ques_6_primenumber {
 		
 		Scanner sc = new Scanner(System.in);
         System.out.println("enter a number");
-        
+        int num = sc.nextInt();
+        int flag= 0 ;
+        for(int i=2;i<=num/2;i++) {
+        	if(num%i==0) {
+        		flag = 1;
+        	}
+        }
+        if(flag==0) {
+        	System.out.println("prime");
+        }
 
 	}
 
